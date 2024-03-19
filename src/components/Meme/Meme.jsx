fb0de4d8849489074dc59121caf7ce448ecc543d
kellypride <ngehkellypride181@gmail.com>
2024-03-19 10:39:37 +0100

    using useState to render pictures on the screen.

--- src/components/Meme/Meme.jsx
+++ src/components/Meme/Meme.jsx
@@ -2,14 +2,17 @@ import React from "react";
 import memesData from "../../memesData";
 
 export default function Meme () {
+    const [memeImage, setMemeImage] = React.useState("")
+    
 
     let url
 
     function getMemeImage (){
         const memeArray = memesData.data.memes
         const randomNumber = Math.floor(Math.random() * memeArray.length)
-        url = memeArray[randomNumber].url
-        console.log(url)
+        console.log("memeArray[randomNumber].url", memeArray[randomNumber].url);
+        setMemeImage(memeArray[randomNumber].url)
+        
     }
     return (
         <main className="px-10 flex flex-col justify-center gap-10 py-10  bg-white font-karla">
@@ -26,11 +29,12 @@ export default function Meme () {
                     placeholder="And take your money"
                     className="border border-gray-400 rounded-lg py-2 indent-3"
                 />
-                
-            </div>
-            <button className=" bg-purple-800 text-white rounded-lg px-4 py-2 cursor-pointer border-none" onClick={getMemeImage} >
+                <button className=" bg-purple-800 col-span-2 text-white rounded-lg px-4 py-2 cursor-pointer border-none" onClick={getMemeImage} >
                     Get a new meme image 🖼
             </button>
+            </div>
+            {memeImage && <img src={require(`../../${memeImage}`)} alt="memes img"  />}
+            
         </main>
     )
 }
--- src/memesData.jsx
+++ src/memesData.jsx
@@ -1,45 +1,41 @@
-const memesData =  {
-  success: true, // Change to false if there's an error
+const memesData = {
+  success: true, 
   data: {
     memes: [
       {
-        id: "1",
+        id: "1", 
         name: "Drake Hotline Bling",
-        url: "src/assets/memeimg.png",
+        url: "assets/memeimg.png",
         width: 1200,
         height: 1200,
         box_count: 1,
-
       },
       {
         id: "2",
         name: "think wise",
-        url: "src/assets/memes1.png",
+        url: "assets/memes1.png", 
         width: 1200,
         height: 1200,
         box_count: 2,
       },
       {
-        id: "1",
+        id: "3", 
         name: "Two buttons",
-        url: "src/assets/meme3.png",
+        url: "assets/meme3.png",
         width: 1200,
         height: 1200,
         box_count: 3,
       },
       {
-        id: "1",
+        id: "4", // Changed ID to be unique
         name: "one man done",
-        url: "src/assets/memes2.png",
+        url: "assets/memes2.png", 
         width: 1200,
         height: 1200,
         box_count: 4,
       },
-
     ],
   },
 };
 
-
-
-export default memesData
+export default memesData;
